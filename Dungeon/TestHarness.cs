d43f6e3496ef932a15cc8582e67ccd502bd5d243
Muffin <kellan.campbell@outlook.com>
2023-02-21 14:25:37 -0600

    Put in some classes

---
+++ Dungeon/TestHarness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    internal class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            //Build and test the functionality of our Library.
+            //Build and test a weapon
+            //Build and test a Character - test all methods
+
+
+
+
+        }//end Main()
+    }//end class
+}//end namespace
---
+++ DungeonLibrary/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    //Public again
+    public class Weapon
+    {
+        //Fields
+        /*
+         * int for min damage (cannot be less than 0 or > max)
+         * int for max damage (cannot be less than 0) - assign first in ctor
+         * string for the name
+         * int for bonusHitChance
+         * bool isTwoHanded
+         * 
+         */
+        //Properties - 1 for each field
+
+        //Constructors
+        //1 fully qualified ctor, and 1 unqualified
+
+        //Methods
+        //Nicely formatted ToString() override
+
+
+
+
+
+    }//end class
+}//end namespace
