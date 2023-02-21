60f209f6ae6f9c507e730933351dbdc7b265892b
Muffin <kellan.campbell@outlook.com>
2023-02-21 08:58:41 -0600

    The beginning

--- Dungeon/Program.cs
+++ Dungeon/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //TODO Player creation
+            
+            //TODO monster and room do while loop
+
+            //TODO player action do while loop
+
+            //Menus
+            /*
+             * attack - if win generate new monster and room, if loose leave both loops
+             * run away - leave inner loop and monster gets a free attack
+             * charachter info
+             * monster info
+             * 
+             */
+
+
         }
     }
 }
