a4672321f27fe741287093065c2e8e33205e1e32
Muffin <kellan.campbell@outlook.com>
2023-02-22 08:31:54 -0600

    Homework Complete

--- Dungeon/TestHarness.cs
+++ Dungeon/TestHarness.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using DungeonLibrary;
 
 namespace Dungeon
 {
@@ -15,6 +18,31 @@ namespace Dungeon
             //Build and test a Character - test all methods
 
 
+
+            //Weapon test
+            Weapon firstWeapon = new Weapon();
+            firstWeapon.MaxDamage = 10;
+            firstWeapon.MinDamage = 20;
+            firstWeapon.Name = "Best Friend Sword";
+            firstWeapon.BonusHitChance = 10;
+            firstWeapon.IsTwoHanded = true;
+
+            Console.WriteLine(firstWeapon);
+
+            Console.WriteLine();
+
+
+
+            //Character test
+            Character proto = new Character(50, 60, "Proto", 70,15);
+
+            Console.WriteLine(proto);
+            Console.WriteLine();
+            //Methods test
+            Console.WriteLine("Testing Methods: \n" +
+                $"Whats the block? {proto.CalcBlock()}\n" +
+                $"What about DAMAGE? {proto.CalcDamage()}\n" +
+                $"But how likely is it to hit? {proto.CalcHitChance()}");
 
 
         }//end Main()
